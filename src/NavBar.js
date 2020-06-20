8b0826ff3c2bb76fc545b72c856939faf8798e75
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-06-20 18:10:03 +0100

    plays with navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-
 const NavBar = (props) => {
   return (
     <header>
+
       <ul>
         <li className="navLink">
-          <a href="/stories">News Articles</a>
+          <a href="/home">Home</a>
         </li>
+        <li className="navLink">
+          <a href="/about">About</a>
+        </li>
+        <li className="navLink">
+          <a href="/news">News</a>
+        </li>
+
       </ul>
     </header>
   )
