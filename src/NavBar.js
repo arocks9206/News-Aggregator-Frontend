9148b60c787dd4d78b5437dff59e595de27771f9
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-06-20 23:35:45 +0100

    adds searchbar functionality

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+    constructor(props){
+      super(props);
+        this.state = {
+          searchbar: []
+        }
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+  handleChange(e) {
+      let currentList = [];
+      let newList = [];
+    if (e.target.value !== "") {
+      currentList = this.props.items;
+      newList = currentList.filter(item => {
+        const lc = item.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter);
+      });
+    } else {
+      newList = this.props.items;
+    }
+    this.setState({
+      filtered: newList
+    });
+  }
 
 
+ render(){
+   return (
+     <header>
+        <img src="/images/logo.png" alt="Logo"/>
 
-const NavBar = (props) => {
-  return (
-    <header>
-    <img src="/images/logo.png" alt="Logo"/>
+        <ul>
+          <li className="navLink">
+            <a href="/home">Home</a>
+          </li>
+          <li className="navLink">
+            <a href="/stories">News Articles</a>
+          </li>
+          <li className="navLink">
+            <a href="/about">About Us</a>
+          </li>
+        </ul>
 
-      <ul>
-      <li className="navLink">
-        <a href="/home">Home</a>
-      </li>
-        <li className="navLink">
-          <a href="/stories">News Articles</a>
-        </li>
-        <li className="navLink">
-          <a href="/about">About Us</a>
-        </li>
-      </ul>
-
-      <div className="searchbar">
-        <input type="text" className="input" placeholder="Search..." />
-      </div>
-    </header>
-  )
+        <div className="searchbar">
+          <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
+        </div>
+      </header>
+    )
+  }
 }
-
 export default NavBar;
