d6aeeb8ae4c569a1aede63b3ea91bd45641a8573
Jack Quigley <jackaquigley@gmail.com>
2020-06-23 11:28:26 +0100

    sorted out html/json error

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -3,9 +3,35 @@ import React, {Component} from 'react';
 import PublisherList from '../components/publishers/PublisherList.js';
 import SourceList from '../components/sources/SourceList.js';
 import StoryList from '../components/stories/StoryList.js';
+import Request from '../helpers/request.js'
 
 
 class NewsContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stories: [],
+      sources: [],
+      publishers: []
+    }
+  }
+
+  componentDidMount(){
+    const request = new Request();
+    const storyPromise = request.get('/api/stories')
+    const sourcePromise = request.get('/api/sources')
+    const publisherPromise = request.get('/api/publishers')
+
+    Promise.all([storyPromise, sourcePromise, publisherPromise])
+    .then((data) => {
+      this.setState({
+        stories: data[0],
+        sources: data[1],
+        publishers: data[2]
+      })
+    })
+
+  }
 
   render(){
 
